807b03e958174550a468aa4efcc71947866182de
DDevilISL <brunorbsf@gmail.com>
2018-07-04 17:45:16 -0300

    Added solution

---
+++ Engine/Common/Include/Types.h
@@ -0,0 +1,12 @@
+#pragma once
+#define interface struct;
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long uint64;
+
+typedef char int8;
+typedef short int16;
+typedef int int32;
+typedef long int64;
